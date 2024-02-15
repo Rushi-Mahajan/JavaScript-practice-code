8a3ea0a46490081fb7b3c5f1707c7d941246b781
Hrushikesh Mahajan <43132409+Rushi-Mahajan@users.noreply.github.com>
2024-02-15 12:35:02 +0000

    switchcase

---
+++ javascripts/Conditions Or Control flow/switchCase.js
@@ -0,0 +1,33 @@
+// syntax: SwitchCase
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+let Month="Mar"
+
+switch (Month) {
+    case "Jan":
+        console.log("Jan");
+        break;
+    case "Feb":
+        console.log("Feb");
+        break;
+    case "Mar":
+        console.log("March");
+        break;
+    case "Apr":
+        console.log("Apr");
+        break;
+    case "May":
+        console.log("May");
+        break;
+    default:
+        console.log("Invalid Month");
+        break;
+}
