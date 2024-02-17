38ef390144c8593494eb3ae0aa12a2c2bdc886c5
Hrushikesh Mahajan <43132409+Rushi-Mahajan@users.noreply.github.com>
2024-02-17 16:21:38 +0000

    While & do-while loop

---
+++ javascripts/loops/dowhileloop.js
@@ -0,0 +1,17 @@
+// syntax
+// variable declarations
+// do {
+    // increament or decreament
+// } while (condition);
+
+// dowhile loop -> always iterate one time before checking the conditions
+// In below case, it will print score as 11.
+// let score=11
+
+
+// let score=1
+
+do {
+    console.log(`Score is ${score}`)
+    score++;
+} while (score<=10);
---
+++ javascripts/loops/whileloops.js
@@ -0,0 +1,21 @@
+// syntax
+// variable declarations
+// while (condition) {     
+// Increament/Decreament
+// }
+
+
+// let i=0
+// while (i<=10) {
+//     console.log(`Value of i is ${i}`);
+//     i =i+2
+// }
+
+
+// iterating array using while loop
+let myArray=["superman","batman","Sharks","Flash"]
+let i=0
+while (i< myArray.length) {
+    console.log(myArray[i]);
+    i++;
+}
